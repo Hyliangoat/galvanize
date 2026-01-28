0bf330af5b1dc4db8cc2ccca4fefc58ece6beac7
Hyliangoat <reignoflife@gmail.com>
2026-01-28 12:13:09 -0500

    Did a ton of java practice in sandbox.js

--- .bashrc
+++ .bashrc
@@ -11,3 +11,7 @@ alias gs='git status'
 alias ga='git add'
 alias gc='git commit'
 alias gp='git push'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- projects/week_1/java_practice/sandbox.js
+++ projects/week_1/java_practice/sandbox.js
@@ -1,5 +1,51 @@
 //index.js
 
-const message = 'Hello World!';
 
-console.log(message);
+//${} will print strings better
+//Ex: instead of fname + " " + lname + " is a nerd"
+//Do: `${fname} ${lname} is a nerd`
+
+
+
+function greet(name) {
+    console.log(`Greetings, ${name}!`);
+}
+
+function greetAlot(name1, name2, name3){
+    console.log(`Hello, ${name1}, ${name2}, and ${name3}`);
+}
+
+function addIt(num1, num2){
+    return num1 + num2;
+}
+//Rounding and parsing
+let roundMe = 2.598495;
+let parseMe = "24";
+
+//Formula is Math.random() * (max - min) + min
+let randomNum = Math.random() * (10 - 1) + 1;
+
+//Can apparently assign shit to a function
+let adder = addIt(5, 10);
+
+//Strings
+let testString = "Jordan";
+let firstChar = testString[0];
+let thirdChar = testString[2];
+let lengthOfString = testString.length;
+let subString = testString.substring(3);
+let indexString = testString.indexOf("dan");
+let numTest = 25;
+let stringNum = numTest.toString();
+
+greet("Emperor Purdy");
+greetAlot("Billy Bob", "Jimmy John", "Paulie");
+console.log(addIt(5,10));
+console.log(adder);
+console.log(Math.ceil(roundMe));
+console.log(Number.parseInt(parseMe));
+console.log(randomNum);
+console.log(`${testString} is my name, my first letter is ${firstChar} and my third letter is ${thirdChar}. I'm really glad my name isn't ${subString}`);
+console.log("Name length = " + lengthOfString)
+console.log("Dan is located at " + indexString)
+console.log(stringNum)
